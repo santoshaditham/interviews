e1285861c85b8fe06e59ad56d74b6ea8f8818b7b
santosh <saditham@mail.usf.edu>
2015-07-27 14:13:03 -0700

    Update heapsort.cpp

--- heapsort.cpp
+++ heapsort.cpp
@@ -1,3 +1,4 @@
+/*in-place heap sort*/
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -10,13 +11,14 @@ void swap(vector<int>* input, int i, int j){
 
 void heapify(vector<int>* input, int i, int n){
 	while(i<n){
-		//get max child
-		int check_with=2*i;
-		if(check_with+1 < input->size() && 
-			input->at(check_with+1) > input->at(check_with)) 
+		//does node i have children?
+		if(2*i+1>n) return;
+		//if so, get max child
+		int check_with=2*i+1;
+		if((check_with+1 <= n) && (input->at(check_with+1) > input->at(check_with))) 
 			check_with++;
 		//swap if this > max child	
-		if(input->at(i) > input->at(check_with)){ 
+		if(input->at(i) < input->at(check_with)){
 			swap(input, i, check_with);
 			i=check_with;
 		}
@@ -25,20 +27,26 @@ void heapify(vector<int>* input, int i, int n){
 }
 
 void build_heap(vector<int>* input){
-	for(int i=0; i<input->size()/2; i++) {heapify(input, i, input->size());}
+	int non_leaf = input->size()/2;
+	for(int i=non_leaf; i>=0; i--) {heapify(input, i, input->size()-1);}
+	for(auto& x: *input) cout << x << " ";
+	cout << "built heap" << endl;
 }
 
 void heapsort(vector<int>* input){
 	build_heap(input);
 	int last_idx = input->size()-1;
-	for(int i=last_idx; i>0; i--){
-		swap(input, input->at(i), input->at(0));
-		heapify(input, 0, i);
+	while(last_idx>0){
+		swap(input, last_idx, 0);
+		last_idx--;
+		heapify(input, 0, last_idx);
+		for(auto& x: *input) cout << x << " ";
+		cout << "heapified" << endl;
 	}
 }
 
 int main() {
-	vector<int> input {4,7,2,0,4,2,1,1,7};
+	vector<int> input {19,17,16,12,9,15,1,2,11,7,3,10,14};
 	heapsort(&input);
 	for(auto& x: input){cout << x << " ";}
 	return 0;
