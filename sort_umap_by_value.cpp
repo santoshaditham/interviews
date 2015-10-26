4e4f5cc774edd66a24b19890baf025460c4471bc
santosh <saditham@mail.usf.edu>
2015-10-26 17:33:06 -0400

    Create sort_umap_by_value.cpp

---
+++ sort_umap_by_value.cpp
@@ -0,0 +1,19 @@
+/*sorting an unordered map by value*/
+
+#include <iostream>
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+	std::unordered_map<std::string,int> mymap;
+  	mymap = {{"Australia",1},{"U.S.",3},{"France",2},{"India",3},{"Nepal",1}};
+  	for(auto& x : mymap)
+  		cout << x.first << " " << x.second << endl;
+  	std::multimap<int,std::string, greater<int>> mymap2;
+  	for(auto& x : mymap)
+  		mymap2.emplace(x.second, x.first);
+    for (auto& x : mymap2)
+    	cout << x.first << " " << x.second << endl;
+	return 0;
+}
