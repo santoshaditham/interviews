e1735d50dd739d12f695520cc2fa796df8212214
santosh <saditham@mail.usf.edu>
2015-11-05 21:31:17 -0500

    Update universal_map.cpp

--- universal_map.cpp
+++ universal_map.cpp
@@ -1,19 +1,21 @@
 /* universal map to fit any type of data for key or value*/
 #include <iostream>
 #include <unordered_map>
+#include <string>
 
 class Universal_Map {
-    std::unordered_map<const void *, const void *> umap;
-
+    std::unordered_map<void *, void *> umap;
 public:
-    void put(const void *key, const void *value) { umap[key] = value; }
-    const void *get(const void *key) { return umap[key]; }
+    void put(void *key, void *value) { umap[key] = value;}
+    const void *get(void *key) { return umap[key];}
 };
 
 int main() {
+	int* int_val = new int(1); double* long_val = new double(1.00);
+	char* char_val = new char('a'); std::string* string_val = new std::string("Hello");
     Universal_Map um;
-    um.put("Bob", "Dylan");
-    int one = 1;
-    um.put("one", &one);
-    std::cout << (char *)um.get("Bob") << "; " << *(int *)um.get("one");
+    um.put((void*)char_val, (void*)string_val);
+    um.put((void*)int_val, (void*)long_val);
+    std::cout << (std::string *)um.get(char_val) << std::endl;
+    std::cout << *(double *)um.get(int_val) << std::endl;
 }
