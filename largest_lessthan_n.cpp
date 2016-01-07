0a1c08bc14089c3776a56aeab8d5e6d079d1aa12
santosh <saditham@mail.usf.edu>
2016-01-07 01:32:49 -0500

    Update largest_lessthan_n.cpp

--- largest_lessthan_n.cpp
+++ largest_lessthan_n.cpp
@@ -1,38 +1,34 @@
 /*given a number n, find largest number smaller than n formed using same digits as n*/
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
-int foo(int n){
-	if(n<10) return n;
-	
-	int result=0, temp=n;
-	vector<int> digits;
-	//break number to digits
-	while(temp > 0){
-		digits.push_back(temp % 10);
-		temp = temp/10;
-	}
-	//look for place where adjacent digits can be swapped
-	int prev_max = digits.at(0);
-	int prev_max_pos = 0;
-	for(int i = 1; i < digits.size(); i++){ 
-		if(digits.at(i) > prev_max)
-		{ //swap
-			int k = digits.at(i);
-			digits.at(i) = prev_max;
-			digits.at(prev_max_pos) = k;
-			break;
-		}
-	}
-	//output
-	for(int i = digits.size()-1; i>=0; i--)
-		result = (result*10) + digits.at(i);
-	return result;
+string foo(int n){
+  string str = to_string(n);
+  if(n<10) return str;
+  //start at back and look for 1st occurance of current digit < previous digit
+  bool change = false;
+  unsigned int idx=str.length()-1; 
+  while(idx > 0){ 
+    unsigned int cur = (unsigned int) str.at(idx);
+    unsigned int prev = (unsigned int) str.at(idx-1); //previous
+    if(cur < prev) {
+      change = true;
+      break;
+    }
+    idx--;
+  }
+  // swap digits
+  if(change){
+    char temp = str.at(idx);
+    str.at(idx) = str.at(idx-1);
+    str.at(idx-1) = temp;
+  }
+  return str;
 }
 
 int main() {
-	int n = 4321; //assume n > 0
-	cout << foo(n) << endl;
-	return 0;
+  int n = 34511; //assume n > 0
+  cout << foo(n);
+  return 0;
 }
