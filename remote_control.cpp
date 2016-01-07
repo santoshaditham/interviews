b01d4c2b94fd3d4aa88b21509c4868857a047b52
santosh <saditham@mail.usf.edu>
2016-01-07 00:02:37 -0500

    Update remote_control.cpp

--- remote_control.cpp
+++ remote_control.cpp
@@ -8,37 +8,36 @@ you would want the output of the program to say
 #include <vector>
 using namespace std;
 
-void find(char c, int* cur_pos){
-	int idx = c - 'a';//ascii
-
-	while(*cur_pos!=idx){
-		if(*cur_pos/3 < idx/3){//go row below
-			*cur_pos+=3;
-			cout << "down ";
-		}
-		else if(*cur_pos/3 > idx/3){//go row above
-			*cur_pos-=3;
-			cout << "up ";
-		}
-		else{//same row
-			if(*cur_pos<idx) {
-				for(int i=0; i<(idx - *cur_pos); i++) cout << "right ";
-				*cur_pos += (idx - *cur_pos); 
-			}
-			if (*cur_pos>idx) {
-				for(int i=0; i<(*cur_pos - idx); i++) cout << "left ";
-				*cur_pos -= (*cur_pos - idx); 
-			}
-		}
-	}
-	cout << "PRESS" << endl;
+void find(char c, int& cur_pos){
+  int idx = c - 'a';//ascii
+  while(cur_pos!=idx){
+    if(cur_pos/3 < idx/3){//go row below
+      cur_pos+=3;
+      cout << "down ";
+    }
+    else if(cur_pos/3 > idx/3){//go row above
+      cur_pos-=3;
+      cout << "up ";
+    }
+    else{//same row
+      if(cur_pos<idx) {
+        for(int i=0; i<(idx - cur_pos); i++) cout << "right ";
+        cur_pos += (idx - cur_pos); 
+      }
+      if (cur_pos>idx) {
+        for(int i=0; i<(cur_pos - idx); i++) cout << "left ";
+        cur_pos -= (cur_pos - idx); 
+      }
+    }
+  }
+  cout << "PRESS" << endl;
 }
 
 int main(){
-	string s = "feed";
-	int cur_pos=0;
-	for(auto x: s){
-		find(x, &cur_pos);
-	}
-	return 0;
+  string str = "feed";
+  int cur_pos=0;
+  for(auto& x: str){
+    find(x, cur_pos);
+  }
+  return 0;
 }
