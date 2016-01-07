e49501452e3f44194a7a52a9bdb56fc19d5fd1a8
santosh <saditham@mail.usf.edu>
2016-01-07 13:04:26 -0500

    Update longest_palindrome.cpp

--- longest_palindrome.cpp
+++ longest_palindrome.cpp
@@ -1,38 +1,38 @@
-/* longest palindrome within a given string input */
+/* length of longest palindrome within a given string input */
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-//input can be any size
-//we can use a suffix tree or Manacher algorithm for better time complexity - O(n)
-//but neither of them is more space efficient than this method
+// we can use a suffix tree - O(nlogn) or Manacher algorithm - O(n) for better time complexity 
+// but neither of them is more space efficient than this method - O(n2) time and O(1) space 
 
 int longestPalindrome(string s, int center_this){
-	int left_this = center_this-1;
-	int right_this = center_this+1;
-	while(s[left_this]==s[right_this]){//palindrome
-		left_this--;right_this++;
-	}
-	return right_this-left_this-1;
+  int left_this = center_this-1;
+  int right_this = center_this+1;
+  while(s[left_this]==s[right_this]){//palindrome
+    left_this--;right_this++;
+  }
+  return right_this-left_this-1;
 }
 
 int main() {
-	string input;
-	int result=0;
-	cin >> input;
-	if(input.size() == 2)
-		cout << (input[0]==input[1])?2:0;
-	if(input.size()>2){
-		int i = input.size()/2; 
-		int left = (input.size()%2!=0)?i:i-1;//even or odd
-		int right = i;
-		while(i>0){
-			int l_result = longestPalindrome(input, left); result = (result>l_result)?result:l_result;
-			int r_result = longestPalindrome(input, right); result = (result>r_result)?result:r_result; 
-			left--;right++;i--;
-		}
-		cout << "longest palindrome is of size: " << result << endl;
-	}
-	return 0;
+  string input = "abaaba";
+  int result=0;
+  if(input.size() == 2)
+    (input[0]==input[1]) ? cout << 2 : cout << 0;
+  if(input.size()>2){
+    int i = input.size()/2; //middle
+    int left = (input.size()%2!=0)?i:i-1; //even or odd
+    int right = i;
+    while(i>0){ //start at middle and check palindromes on both sides
+      int l_result = longestPalindrome(input, left); 
+      result = (result>l_result)?result:l_result;
+      int r_result = longestPalindrome(input, right); 
+      result = (result>r_result)?result:r_result; 
+      left--; right++; i--;
+    }
+    cout << "longest palindrome is of size: " << result << endl;
+  }
+  return 0;
 }
