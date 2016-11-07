75cfb48df45688f482037cbd2cf716b394f71dd8
santosh <saditham@mail.usf.edu>
2016-11-07 00:57:07 -0500

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -4,73 +4,127 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
+#include <queue>
 
 using namespace std;
 
-struct bst_node{
+struct node{
     int data;
-    bst_node* right;
-    bst_node* left;
+    node* right;
+    node* left;
 }bst_root;
 
-void bst_insert(bst_node* _node, int d){
+void bst_insert(node* bnode, int d){
     //temp node
-    bst_node* temp = new bst_node;
+    node* temp = new node;
     temp->data=d;
     temp->right=NULL;
     temp->left=NULL;
     
     //empty BST
-    if(_node==NULL){
-        _node=temp;
+    if(bnode==NULL){
+        bnode=temp;
         return;
     }
     
     //right subtree
-    if(d > _node->data){
-        if(_node->right==NULL){
-            _node->right=temp;
+    if(d > bnode->data){
+        if(bnode->right==NULL){
+            bnode->right=temp;
             return;
         }
         else
-            bst_insert(_node->right, d);
+            bst_insert(bnode->right, d);
     }
     
     //left subtree
-    if(d < _node->data){
-        if(_node->left==NULL){
-            _node->left=temp;
+    if(d < bnode->data){
+        if(bnode->left==NULL){
+            bnode->left=temp;
             return;
         }
         else
-            bst_insert(_node->left, d);
+            bst_insert(bnode->left, d);
     }
 
 }
 
-void print_preorder(bst_node* _node){
-    if(_node->left!=NULL)
-        print_preorder(_node->left);
-    cout << _node->data << " ";
-    if(_node->right!=NULL)
-        print_preorder(_node->right);   
+node* find(node* root, int d){
+	if(root==NULL) return root;
+	if(root->data > d) find(root->left, d);
+	else if(root->data < d) find(root->right, d);
+	else return root;
 }
 
-bool is_bst(bst_node* bnode, int min, int max){
+int height(node* root, node* n){
+	if(root==NULL) return 0;
+	if(root->data > n->data) return 1+ height(root->left, n);
+	else if(root->data < n->data) return 1+ height(root->right, n);
+	else return 0;	
+}
+
+void print_preorder(node* bnode){
+    if(bnode->left!=NULL)
+        print_preorder(bnode->left);
+    cout << bnode->data << " ";
+    if(bnode->right!=NULL)
+        print_preorder(bnode->right);   
+}
+
+void print_inorder(node* bnode){
+    cout << bnode->data << " ";
+    if(bnode->left!=NULL)
+        print_inorder(bnode->left);
+    if(bnode->right!=NULL)
+        print_inorder(bnode->right); 
+}
+
+bool is_bst(node* bnode, int min, int max){
 	if(bnode==NULL) return true;
 	if(bnode->data < min || bnode->data > max) return false;
 	return is_bst(bnode->left, min, bnode->data) && is_bst(bnode->right, bnode->data, max);
 }
 
+int lca(node* root, int n1, int n2){
+	if(root==NULL) return -1;
+	if(root->data > n1 && root->data > n2) lca(root->left, n1, n2);
+	else if(root->data < n1 && root->data < n2) lca(root->right, n1, n2);
+	else return root->data;
+}
+
+bool is_subtree(node* n1, node* n2){
+	node* temp = find(n1, n2->data); 
+	return (n2 == temp);
+}
+
+void print_by_level(node* root){
+	if(root==NULL) return;
+	queue<node*> my_q;
+	my_q.push(root);
+	int prev = 0;
+	while(!my_q.empty()){
+		if(height(root, my_q.front()) != prev) cout << endl; 
+		prev = height(root, my_q.front()); 
+		cout << my_q.front()->data << " ";
+		if(my_q.front()->left != NULL) my_q.push(my_q.front()->left);
+		if(my_q.front()->right != NULL) my_q.push(my_q.front()->right);
+		my_q.pop();
+	}
+}
+
 int main()
 {
-   vector<int> input = {1,10,6,4,5};
-
-   for(auto& x: input){
+   vector<int> input = {2,10,6,4,5,1,3,12,11,13};
+   for(auto& x: input)
        bst_insert(&bst_root, x);
-   }
 
-   print_preorder(&bst_root);
-   cout << is_bst(&bst_root, INT_MIN, INT_MAX); 
+   cout << "pre-order: " << endl; print_preorder(&bst_root); cout << endl; // non decreasing
+   cout << "in-order: " << endl; print_inorder(&bst_root); cout << endl;
+   cout << "this a BST: " << is_bst(&bst_root, INT_MIN, INT_MAX) << endl; 
+   cout << "lca(3,12): " << lca(&bst_root,3,12) << endl;
+   node* n1 = find(&bst_root, 2);
+   node* n2 = find(&bst_root, 4);
+   cout << "tree from node(" << n2->data << ") is subtree of tree from node(" << n1->data << ") : " << is_subtree(n1, n2) << endl; 
+   cout << "print by level: " << endl; print_by_level(&bst_root);
    return 0;
 }
